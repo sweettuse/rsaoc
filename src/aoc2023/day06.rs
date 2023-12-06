8e03278f640e354ee054ef92fb0c73de640da8cb
adam cushner <sweettuse@gmail.com>
2023-12-06 18:50:47 -0500

    minor cleanup

--- src/aoc2023/day06.rs
+++ src/aoc2023/day06.rs
@@ -8,7 +8,9 @@ pub fn main() -> (i64, i64) {
 }
 
 fn part1(fname: &str) -> i64 {
-    let (times, distances) = _get_data(fname);
+    let lines = read_file23(fname);
+    let (l1, l2) = lines.iter().collect_tuple().unwrap();
+    let (times, distances) = (_parse_ints(l1), _parse_ints(l2));
     _calc_total_ways(&times, &distances)
 }
 
@@ -30,12 +32,6 @@ fn _calc_total_ways(times: &[i64], distances: &[i64]) -> i64 {
         .zip(distances.iter().copied())
         .map(|(t, d)| _calc_ways(t, d))
         .product::<i64>()
-    //     let (min, max) = _calc_roots(t, d);
-    //     let offset = _get_ends_offset(t, min, d) + _get_ends_offset(t, max, d);
-    //     let res = max - min + 1 - offset;
-    //     total_ways *= res;
-    // }
-    // total_ways
 }
 
 fn _calc_ways(t: i64, d: i64) -> i64 {
@@ -55,12 +51,7 @@ fn _get_ends_offset(t_total: i64, t_charge: i64, dist: i64) -> i64 {
 /// distance = (t_total - t_charge) * t_charge
 /// t_charge ** 2 - t_total * t_charge + distance
 fn _calc_roots(t: i64, d: i64) -> (i64, i64) {
-    let inner = ((t.pow(2) - 4 * d) as f64).sqrt();
-    let t = t as f64;
-
-    let r1 = (t + inner) / 2.;
-    let r2 = (t - inner) / 2.;
-
+    let (r1, r2) = _calc_roots_float(t, d);
     (r1.min(r2).ceil() as i64, r1.max(r2).floor() as i64)
 }
 
@@ -73,11 +64,7 @@ fn _calc_roots_float(t: i64, d: i64) -> (f64, f64) {
 
     (r1.min(r2), r1.max(r2))
 }
-fn _get_data(fname: &str) -> (Vec<i64>, Vec<i64>) {
-    let lines = read_file23(fname);
-    let (l1, l2) = lines.iter().collect_tuple().unwrap();
-    (_parse_ints(l1), _parse_ints(l2))
-}
+
 
 fn _parse_ints(s: &str) -> Vec<i64> {
     s.split(' ').filter_map(|v| v.parse::<i64>().ok()).collect()
