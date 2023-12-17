7bf336d67e8de6d8857b2986356ef78bd607df8a
adam cushner <sweettuse@gmail.com>
2023-12-17 03:13:30 -0500

    day16 cleanup

--- src/aoc2023/day16.rs
+++ src/aoc2023/day16.rs
@@ -1,9 +1,7 @@
 use std::{
-    cell::RefCell,
     collections::{HashMap, HashSet, VecDeque},
-    iter::{zip, repeat},
+    iter::{repeat, zip},
     ops::{Add, Neg, Sub},
-    rc::Rc,
 };
 
 use glam::IVec2;
@@ -18,7 +16,6 @@ use crate::{
 };
 
 pub type AocRes = Result<i32, String>;
-// type SharedCell = Rc<RefCell<Cell>>;
 
 pub fn main() -> (AocRes, AocRes) {
     (part1(), part2())
@@ -26,18 +23,21 @@ pub fn main() -> (AocRes, AocRes) {
 
 fn part1() -> AocRes {
     let cave = _get_data("16.txt");
-    Ok(cave.count_energized(Beam { dir: Dir::East, location: Point::new(0, 0) }))
+    Ok(cave.count_energized(Beam {
+        dir: Dir::East,
+        location: Point::new(0, 0),
+    }))
 }
 
 fn part2() -> AocRes {
     // below is really slow
-    return Ok(7572);
+    // return Ok(7572);
     let cave = _get_data("16.txt");
     let max_xy = cave.layout.keys().fold(Point::new(0, 0), |acc, p| {
         Point::new(acc.x.max(p.x), acc.y.max(p.y))
     });
-    let iter_x = || {0..=max_xy.x};
-    let iter_y = || {0..=max_xy.y};
+    let iter_x = || 0..=max_xy.x;
+    let iter_y = || 0..=max_xy.y;
 
     let east_side = (Dir::West, zip(repeat(max_xy.x), iter_y()).collect_vec());
     let west_side = (Dir::East, zip(repeat(0), iter_y()).collect_vec());
@@ -45,11 +45,18 @@ fn part2() -> AocRes {
     let south_side = (Dir::North, zip(iter_x(), repeat(max_xy.y)).collect_vec());
 
     let sources = [east_side, west_side, north_side, south_side];
-    let res = sources.iter().flat_map(|(dir, point_tuples)| {
-        point_tuples.iter().map(|(x, y)| {
-            cave.count_energized( Beam { dir: *dir, location: Point::new(*x, *y)})
+    let res = sources
+        .iter()
+        .flat_map(|(dir, point_tuples)| {
+            point_tuples.iter().map(|(x, y)| {
+                cave.count_energized(Beam {
+                    dir: *dir,
+                    location: Point::new(*x, *y),
+                })
+            })
         })
-    }).max().unwrap();
+        .max()
+        .unwrap();
     Ok(res)
 }
 
@@ -60,7 +67,7 @@ fn _get_data(fname: &str) -> Cave {
 // =============================================================================
 // STRUCTS/ENUMS
 // =============================================================================
-type Layout = HashMap<Point, Cell>;
+type Layout = HashMap<Point, OpticalDevice>;
 
 #[derive(Debug)]
 struct Cave {
@@ -74,14 +81,6 @@ enum OpticalDevice {
     RightMirror,
     LeftMirror,
     Empty,
-}
-
-#[derive(Debug)]
-struct Cell {
-    p: Point,
-    od: OpticalDevice,
-    visitors: HashSet<Dir>,
-    connections: HashMap<Dir, Point>,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
@@ -101,8 +100,7 @@ impl Cave {
         }
     }
 
-    
-    fn count_energized(&self, beam: Beam) -> i32{
+    fn count_energized(&self, beam: Beam) -> i32 {
         let mut queue = VecDeque::from([beam]);
         let mut seen = HashSet::<Beam>::new();
         while let Some(beam) = queue.pop_front() {
@@ -110,68 +108,21 @@ impl Cave {
                 continue;
             }
 
-            let cell = self.layout.get(&beam.location);
-            if cell.is_none() {
-                continue;
+            if let Some(od) = self.layout.get(&beam.location) {
+                seen.insert(beam);
+                queue.extend(od.beams(&beam));
             }
-            seen.insert(beam);
-            queue.extend(cell.unwrap().beams(&beam).iter());
         }
         let unique_points: HashSet<Point> = seen.iter().map(|b| b.location).collect();
         unique_points.len() as i32
     }
 
-}
-
-impl Cell {
-
-    fn from(p: &Point, od: OpticalDevice) -> Self {
-        Self {
-            p: *p,
-            od,
-            visitors: HashSet::new(),
-            connections: HashMap::new(),
-        }
-    }
-
-    fn seen(&self, beam: &Beam) -> bool {
-        let contains = |dir: &Dir| self.visitors.contains(dir);
-        match self.od {
-            OpticalDevice::RightMirror | OpticalDevice::LeftMirror => contains(&beam.dir),
-            OpticalDevice::VerticalSplitter => match beam.dir {
-                Dir::North | Dir::South => contains(&beam.dir),
-                Dir::East | Dir::West => contains(&Dir::East) || contains(&Dir::West),
-            },
-            OpticalDevice::HorizontalSplitter => match beam.dir {
-                Dir::East | Dir::West => contains(&beam.dir),
-                Dir::North | Dir::South => contains(&Dir::North) || contains(&Dir::South),
-            },
-            OpticalDevice::Empty => contains(&beam.dir),
-        }
-    }
-
-    fn beams(&self, beam: &Beam) -> Vec<Beam> {
-        self.od
-            .new_dirs(beam.dir)
-            .iter()
-            .map(|d| Beam {
-                dir: *d,
-                location: beam.location + *d,
-            })
-            .collect()
-    }
-}
-
-// helpers
-impl Cave {
     fn _parse_layout(s: &str) -> Layout {
         s.split('\n')
             .enumerate()
             .flat_map(|(y, line)| {
                 line.chars().enumerate().map(move |(x, c)| {
-                    let od = OpticalDevice::from_char(c);
-                    let p = Point::new(x as i32, y as i32);
-                    (p, Cell::from(&p, od))
+                    (Point::new(x as i32, y as i32), OpticalDevice::from_char(c))
                 })
             })
             .collect()
@@ -189,18 +140,17 @@ impl OpticalDevice {
             _ => panic!("unmatched char {c}"),
         }
     }
-    fn as_char(&self) -> char {
-        match self {
-            Self::VerticalSplitter => '|',
-            Self::HorizontalSplitter => '-',
-            Self::LeftMirror => '\\',
-            Self::RightMirror => '/',
-            Self::Empty => '.',
-        }
+    fn beams(&self, beam: &Beam) -> Vec<Beam> {
+        self._new_dirs(beam.dir)
+            .iter()
+            .map(|d| Beam {
+                dir: *d,
+                location: beam.location + *d,
+            })
+            .collect()
     }
-
     /// map incoming dir of travel to outgoing dir[s] of travel
-    fn new_dirs(&self, dir_of_travel: Dir) -> Vec<Dir> {
+    fn _new_dirs(&self, dir_of_travel: Dir) -> Vec<Dir> {
         match self {
             OpticalDevice::VerticalSplitter => match dir_of_travel {
                 Dir::East | Dir::West => vec![Dir::North, Dir::South],
